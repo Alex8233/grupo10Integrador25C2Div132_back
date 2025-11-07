129182deeda9d92b0828244a6c45ce224d1c0db8
Alex8233 <alexzalazarduran24@gmail.com.>
2025-11-07 20:24:11 -0300

    7/11

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ import cors from "cors";
 //     console.log(`Servidor corriendo desde el puerto ${PORT}`);
 // })
 app.use(cors());
+app.use(express.json());
 app.get("/", (req, res) => {
     res.send("TP Integrador Div 132");
 });
@@ -19,7 +20,7 @@ app.get("/productos", async (req, res) => {
     try {
         const sql = "SELECT * FROM productos";
         const [rows, fields] = await connection.query(sql);
-        console.log(rows);
+       
         res.status(200).json({
             payload: rows
         });
@@ -31,6 +32,26 @@ app.get("/productos", async (req, res) => {
         });
     }
 });
+app.get("/productos/:id_producto", async(req,res)=>{
+    try{
+        console.log(req.params);
+        let {id_producto} = req.params;
+        let sql = "SELECT * FROM productos WHERE productos.id_producto = ?";
+
+        const [rows] = await connection.query(sql, [id_producto]);
+
+        res.status(200).json({
+            payload: rows
+        });
+    }catch(error){
+        console.log("Error obteniendo producto por id: ", error);
+
+        res.status(500).json({
+            message: "Error interno del servidor",
+            error: error.message
+        })
+    }
+})
 app.listen(PORT, () => {
     console.log(`Servidor corriendo desde el puerto ${PORT}`)
 });

cb1d266fcd93afc6e72a7252ccf561c211038e99
Alex8233 <alexzalazarduran24@gmail.com.>
2025-11-11 18:22:41 -0300

    h

--- index.js
+++ index.js
@@ -32,13 +32,13 @@ app.get("/productos", async (req, res) => {
         });
     }
 });
-app.get("/productos/:id_producto", async(req,res)=>{
+app.get("/productos/:id", async(req,res)=>{
     try{
-        console.log(req.params);
-        let {id_producto} = req.params;
-        let sql = "SELECT * FROM productos WHERE productos.id_producto = ?";
 
-        const [rows] = await connection.query(sql, [id_producto]);
+        let {id} = req.params;
+        let sql = "SELECT * FROM productos WHERE productos.id = ?";
+
+        const [rows] = await connection.query(sql, [id]);
 
         res.status(200).json({
             payload: rows
@@ -50,6 +50,30 @@ app.get("/productos/:id_producto", async(req,res)=>{
             message: "Error interno del servidor",
             error: error.message
         })
+    }
+})
+app.post("/productos", async (req, res)=>{
+    try{
+        let{nombre,img_url, tipo, precio} =req.body;
+        console.log(req.body);
+         console.log(`Nombre producto: ${nombre}`);
+
+        let sql = "INSERT INTO productos (nombre, img_url, tipo, precio) VALUES (?, ?, ?, ?)";
+
+        let [rows] = await connection.query(sql, [nombre, img_url, tipo, precio]);
+        console.log(rows);
+        res.status(201).json({
+            message: "Producto creado con exito!",
+        });
+
+
+    }catch(error){
+        console.log("Error al crear producto: ", error);
+        res.status(500).json({
+            message:"Error interno del servidor",
+            error: error.message
+        })
+       
     }
 })
 app.listen(PORT, () => {
